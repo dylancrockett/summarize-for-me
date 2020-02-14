a6a16ae3a4152a14c6db96e0e03e747fc782c4e4
dylancrockett <dylanrcrockett@gmail.com>
2020-02-13 23:08:35 -0700

    init content commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn --no-sendfile --worker-class eventlet -w 1 app:app
---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.summarizers.lex_rank import LexRankSummarizer
+import nltk
+
+app = Flask(__name__)
+
+socket_io = SocketIO(app)
+
+print("DOWNLOADING PUNKT")
+nltk.download('punkt')
+print("FINISHED DOWNLOADING")
+
+
+@app.route('/')
+def hello_world():
+    return render_template("summarize.html")
+
+
+@socket_io.on("summarize")
+def summarize(data):
+    # use sumy to summarize the text entered
+    summarizer = LexRankSummarizer()
+    parser = PlaintextParser.from_string(data["text"], Tokenizer("english"))
+    summary = summarizer(parser.document, data["lines"])
+
+    # formatted summary
+    text = "\t"
+
+    # extract and format the text from the summarizer
+    counter = 0
+    for line in summary:
+        if counter >= 5:
+            text += "<br><br>\t"
+            counter = 0
+
+        text += str(line) + " "
+        counter += 1
+
+    # respond to the user with the summarized text
+    emit("response", text)
+    pass
+
+
+if __name__ == '__main__':
+    socket_io.run(app)
